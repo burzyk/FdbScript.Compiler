2cc3691a88e25a46b3393188339c8e9e1c3d068a
Pawel Burzynski <p.k.burzynski@gmail.com>
2015-03-30 14:22:51 +0100

    Hello world generation

--- src/main/java/uk/co/directline/mathcompiler/Main.java
+++ src/main/java/uk/co/directline/mathcompiler/Main.java
@@ -20,6 +20,8 @@ public class Main {
         ConstantPoolGen cp = cg.getConstantPool(); // cg creates constant pool
         InstructionList il = new InstructionList();
 
+        int str = cp.addString("Hello World 1");
+
         MethodGen mg = new MethodGen(
                 Constants.ACC_STATIC | Constants.ACC_PUBLIC, // access flags
                 Type.VOID,               // return type
@@ -31,78 +33,22 @@ public class Main {
                 cp);
         InstructionFactory factory = new InstructionFactory(cg);
 
-        ObjectType i_stream = new ObjectType("java.io.InputStream");
-        ObjectType p_stream = new ObjectType("java.io.PrintStream");
+        il.append(factory.createFieldAccess(
+                "java.lang.System",
+                "out",
+                new ObjectType("java.io.PrintStream"),
+                Constants.GETSTATIC));
 
-        il.append(factory.createNew("java.io.BufferedReader"));
-        il.append(InstructionConstants.DUP); // Use predefined constant
-        il.append(factory.createNew("java.io.InputStreamReader"));
-        il.append(InstructionConstants.DUP);
-        il.append(factory.createFieldAccess("java.lang.System", "in", i_stream, Constants.GETSTATIC));
+        il.append(new LDC(str));
+
         il.append(factory.createInvoke(
-                "java.io.InputStreamReader",
-                "<init>",
-                Type.VOID,
-                new Type[] { i_stream },
-                Constants.INVOKESPECIAL));
-        il.append(factory.createInvoke(
-                "java.io.BufferedReader",
-                "<init>",
-                Type.VOID,
-                new Type[] { new ObjectType("java.io.Reader") },
-                Constants.INVOKESPECIAL));
-
-        LocalVariableGen lg = mg.addLocalVariable("in", new ObjectType("java.io.BufferedReader"), null, null);
-        int in = lg.getIndex();
-        lg.setStart(il.append(new ASTORE(in))); // "i" valid from here
-
-        lg = mg.addLocalVariable("name", Type.STRING, null, null);
-        int name = lg.getIndex();
-        il.append(InstructionConstants.ACONST_NULL);
-        lg.setStart(il.append(new ASTORE(name))); // "name" valid from here
-
-        InstructionHandle try_start = il.append(
-                factory.createFieldAccess("java.lang.System", "out", p_stream, Constants.GETSTATIC));
-
-        il.append(new PUSH(cp, "Please enter your name> "));
-        il.append(factory.createInvoke("java.io.PrintStream", "print", Type.VOID,
-                new Type[] { Type.STRING },
-                Constants.INVOKEVIRTUAL));
-        il.append(new ALOAD(in));
-        il.append(factory.createInvoke("java.io.BufferedReader", "readLine",
-                Type.STRING, Type.NO_ARGS,
-                Constants.INVOKEVIRTUAL));
-        il.append(new ASTORE(name));
-
-        GOTO g = new GOTO(null);
-        InstructionHandle try_end = il.append(g);
-
-        InstructionHandle handler = il.append(InstructionConstants.RETURN);
-        mg.addExceptionHandler(try_start, try_end, handler, new ObjectType("java.io.IOException"));
-
-        InstructionHandle ih =
-                il.append(factory.createFieldAccess("java.lang.System", "out", p_stream,
-                        Constants.GETSTATIC));
-        g.setTarget(ih);
-
-
-        il.append(factory.createNew(Type.STRINGBUFFER));
-        il.append(InstructionConstants.DUP);
-        il.append(new PUSH(cp, "Hello, "));
-        il.append(factory.createInvoke("java.lang.StringBuffer", "<init>",
-                Type.VOID, new Type[] { Type.STRING },
-                Constants.INVOKESPECIAL));
-        il.append(new ALOAD(name));
-        il.append(factory.createInvoke("java.lang.StringBuffer", "append",
-                Type.STRINGBUFFER, new Type[] { Type.STRING },
-                Constants.INVOKEVIRTUAL));
-        il.append(factory.createInvoke("java.lang.StringBuffer", "toString",
-                Type.STRING, Type.NO_ARGS,
+                "java.io.PrintStream",
+                "println",
+                Type.VOID, new Type[]{Type.STRING},
                 Constants.INVOKEVIRTUAL));
 
-        il.append(factory.createInvoke("java.io.PrintStream", "println",
-                Type.VOID, new Type[] { Type.STRING },
-                Constants.INVOKEVIRTUAL));
+        il.append(factory.createPrintln("Hello World 2"));
+
         il.append(InstructionConstants.RETURN);
 
         mg.setMaxStack();
