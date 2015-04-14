53e4b303bf52c3b73c05991bcf66242dcdac9c30
Pawel Burzynski <p.k.burzynski@gmail.com>
2015-04-14 17:42:30 +0100

    string concat test

---
+++ src/test/java/com/jpbnetsoftware/fdbscript/compiler/tests/StringTests.java
@@ -0,0 +1,32 @@
+package com.jpbnetsoftware.fdbscript.compiler.tests;
+
+import com.jpbnetsoftware.fdbscript.compiler.tests.helpers.CompilerHelper;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by pawel on 14/04/15.
+ */
+public class StringTests {
+
+    @Test
+    public void constStringTest() throws Exception {
+        Object result = CompilerHelper.compileAndInvoke("Test", "module Test \"ala\"");
+
+        Assert.assertEquals("ala", result);
+    }
+
+    @Test
+    public void concatStringTest() throws Exception {
+        Object result = CompilerHelper.compileAndInvoke("Test", "module Test \"ala\" + \" ma kota\"");
+
+        Assert.assertEquals("ala ma kota", result);
+    }
+
+    @Test
+    public void concatMultipleStringTest() throws Exception {
+        Object result = CompilerHelper.compileAndInvoke("Test", "module Test \"ala\" + \" ma kota\" + \", ola ma asa\"");
+
+        Assert.assertEquals("ala ma kota, ola ma asa", result);
+    }
+}
