0a98c3e3a1dac9a10e8e6ab08b6d45af85802b87
Pawel Burzynski <p.k.burzynski@gmail.com>
2015-04-14 08:35:05 +0100

    invoke context

---
+++ src/main/java/com/jpbnetsoftware/fdbscript/runtime/InvokeContext.java
@@ -0,0 +1,33 @@
+package com.jpbnetsoftware.fdbscript.runtime;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by pawel on 14/04/15.
+ */
+public class InvokeContext {
+
+    private Map<String, Object> locals = new HashMap<String, Object>();
+
+    private InvokeContext parent;
+
+    public InvokeContext(InvokeContext parent) {
+        this.parent = parent;
+    }
+
+    public void defineValue(String name, Object value) {
+        this.locals.put(name, value);
+    }
+
+    public Object getValue(String name) {
+        Object value = this.locals.getOrDefault(name, null);
+        value = value == null && this.parent != null ? this.parent.getValue(name) : value;
+
+        if (value == null) {
+            //TODO: throw no variable defined
+        }
+
+        return value;
+    }
+}
