b09e27f712d6094daabf33324f9ba63dd5522425
Pawel Burzynski <p.k.burzynski@gmail.com>
2015-04-23 08:56:24 +0100

    MapFunction arguments

--- src/main/java/com/jpbnetsoftware/fdbscript/runtime/methods/MapFunction.java
+++ src/main/java/com/jpbnetsoftware/fdbscript/runtime/methods/MapFunction.java
@@ -3,6 +3,7 @@ package com.jpbnetsoftware.fdbscript.runtime.methods;
 import com.jpbnetsoftware.fdbscript.runtime.IInvokable;
 import com.jpbnetsoftware.fdbscript.runtime.InvokeContext;
 import com.jpbnetsoftware.fdbscript.runtime.RuntimeList;
+import com.jpbnetsoftware.fdbscript.runtime.RuntimeMethods;
 
 /**
  * Created by pawel on 22/04/15.
@@ -13,14 +14,14 @@ public class MapFunction implements IInvokable {
     public Object invoke(InvokeContext context) {
         RuntimeList list = (RuntimeList) context.getValue("list");
         IInvokable func = (IInvokable) context.getValue("mapper");
-
+        String[] funcArguments = func.getArguments();
         Object[] mapped = new Object[list.getList().length];
 
         for (int i = 0; i < list.getList().length; i++) {
             InvokeContext ctx = InvokeContext.createChildContext(context);
 
-            ctx.defineValue("$e", list.getList()[i]);
-            ctx.defineValue("$i", i);
+            ctx.defineValue(funcArguments.length == 0 ? "$e" : funcArguments[0], list.getList()[i]);
+            ctx.defineValue(funcArguments.length == 1 ? "$i" : funcArguments[1], i);
 
             mapped[i] = func.invoke(ctx);
         }
