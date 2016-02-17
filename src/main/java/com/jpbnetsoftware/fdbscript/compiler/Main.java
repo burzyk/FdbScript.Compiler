2ec807185687de7f487ad81cecb58ba323766b51
burzyk <p.k.burzynski@gmail.com>
2016-02-17 21:35:10 +0000

    command line compilation

--- src/main/java/com/jpbnetsoftware/fdbscript/compiler/Main.java
+++ src/main/java/com/jpbnetsoftware/fdbscript/compiler/Main.java
@@ -2,18 +2,38 @@ package com.jpbnetsoftware.fdbscript.compiler;
 
 import com.jpbnetsoftware.fdbscript.compiler.impl.FileOutputManager;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
 /**
  * Created by pawel on 30/03/15.
  */
 public class Main {
     public static void main(String[] args) throws Exception {
 
-        String program = "module Fib " +
-                "x = [2]" +
-                "x[0:0]";
+        if (args.length < 1) {
+            System.out.println("Missing input file");
+            return;
+        }
 
-        Compiler.compileModule(program, new FileOutputManager());
+        try {
+            String fileName = args[0];
+            StringBuilder sb = new StringBuilder();
+            String line = null;
 
-        System.out.println("Done");
+            BufferedReader fr = new BufferedReader(new FileReader(new File(fileName)));
+
+            while ((line = fr.readLine()) != null) {
+                sb.append(line + "\n");
+            }
+
+            String outputDirectory = args.length == 2 ? args[1] : ".";
+            Compiler.compileModule(sb.toString(), new FileOutputManager(outputDirectory));
+
+            System.out.println("Done");
+        } catch (Exception ex) {
+            return;
+        }
     }
 }
--- src/main/java/com/jpbnetsoftware/fdbscript/compiler/impl/FileOutputManager.java
+++ src/main/java/com/jpbnetsoftware/fdbscript/compiler/impl/FileOutputManager.java
@@ -2,18 +2,32 @@ package com.jpbnetsoftware.fdbscript.compiler.impl;
 
 import com.jpbnetsoftware.fdbscript.compiler.*;
 
-import java.io.FileNotFoundException;
+import java.io.File;
 import java.io.FileOutputStream;
-import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 /**
  * Created by pawel on 10/04/15.
  */
 public class FileOutputManager implements IOutputManager {
 
+    private String outputDirectory;
+
+    public FileOutputManager(String outputDirectory) {
+        this.outputDirectory = outputDirectory;
+    }
+
     @Override
     public void append(String name, byte[] content) throws Exception {
-        FileOutputStream w = new FileOutputStream(name + ".class");
+
+        File dir = new File(this.outputDirectory);
+
+        if (dir.exists()) {
+            dir.createNewFile();
+        }
+
+        FileOutputStream w = new FileOutputStream(dir.getName() + "\\" + name + ".class");
         w.write(content);
     }
 }
